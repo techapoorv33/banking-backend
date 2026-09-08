916ffadd6016dcea52cddfb98e8fc6d0ea40cace
techapoorv33 <techapoorv33@gmail.com>
2026-09-09 01:37:49 +0530

    get balance api added

--- src/controllers/account.controller.js
+++ src/controllers/account.controller.js
@@ -27,4 +27,32 @@ async function getUserAccountController(req, res){
     })
 }
 
-module.exports = {createAccountController, getUserAccountController};
+
+async function getAccountBalanceController(req, res){
+    const { accountId }= req.params;
+
+    const account = await accountModel.findOne({
+        _id: accountId,
+        user: req.user._id
+    })
+
+    if(!account){
+        return res.status(404).json({
+            message: "Account not found"
+        })
+    }
+
+    const balance = await account.getBalance();
+
+    res.status(200).json({
+        accountId: account._id,
+        balance: balance
+    })
+}
+
+module.exports = {
+    createAccountController,
+    getUserAccountController,
+    getAccountBalanceController
+};
+
